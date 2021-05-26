4f98ee57cfff2b646d18fce1af40aff74d928d31
Ameer Albahem <ameer.albahem@gmail.com>
2021-05-26 23:29:42 +1000

    First working version of Uima Integration

---
+++ src/main/java/com/readbiomed/spark/uima/UIMACasDataType.java
@@ -0,0 +1,16 @@
+package com.readbiomed.spark.uima;
+
+import org.apache.spark.sql.types.BinaryType;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.UserDefinedType;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.admin.CASFactory;
+import org.apache.uima.cas.impl.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.Serializable;
+
+public class UIMACasDataType implements Serializable{
+
+}
---
+++ src/main/java/com/readbiomed/spark/uima/UIMAModel.java
@@ -0,0 +1,81 @@
+package com.readbiomed.spark.uima;
+
+import com.google.common.base.Joiner;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.Transformer;
+import org.apache.spark.ml.clustering.LDAModel;
+import org.apache.spark.ml.param.ParamMap;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.api.java.UDF4;
+import org.apache.spark.sql.catalyst.encoders.RowEncoder;
+import org.apache.spark.sql.catalyst.expressions.Encode;
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
+import org.apache.spark.sql.types.*;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.cas.TypeClass;
+import org.apache.uima.fit.factory.AggregateBuilder;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import java.util.*;
+
+public class UIMAModel extends Transformer {
+
+    private Map<String,Class> outColTypeClazzMap;
+
+    public void setTypes(Map<String, Class> types) {
+        this.outColTypeClazzMap = types;
+    }
+
+    @Override
+    public StructType transformSchema(StructType schema) {
+        return schema;
+    }
+
+    @Override
+    public Dataset<Row> transform(Dataset<?> dataset) {
+            Dataset<Row> rowDataset = (Dataset<Row>) dataset;
+            rowDataset = rowDataset.map((MapFunction<Row, Row>) row -> {
+                AggregateBuilder aggregateBuilder = new AggregateBuilder();
+                final AnalysisEngine analysisEngine = aggregateBuilder.createAggregate();
+                String textValue = row.<String>getAs("text").toString();
+                final JCas jCas = JCasFactory.createText(textValue);
+                analysisEngine.process(jCas);
+
+
+                final List<Object> values = new ArrayList<>();
+
+                final ArrayList<StructField> structFields = new ArrayList<>(Arrays.asList(row.schema().fields()));
+                for (int i = 0; i < structFields.size(); i++) {
+                    values.add(row.get(i));
+                }
+
+                outColTypeClazzMap.keySet().stream().forEach(annotation -> {
+                    StructField structField =  StructField.apply(annotation, DataTypes.createArrayType(DataTypes.StringType),true,Metadata.empty());
+                    structFields.add(structField);
+                });
+                outColTypeClazzMap.entrySet().stream().forEach(entry -> {
+                        values.add(JCasUtil.toText(JCasUtil.select(jCas, entry.getValue())).toArray(new String[1]));
+                });
+
+                return new GenericRowWithSchema(values.toArray(), new StructType(structFields.toArray(new StructField[1])));
+            }, Encoders.kryo(Row.class));
+            return rowDataset;
+        }
+
+    @Override
+    public Transformer copy(ParamMap extra) {
+        return defaultCopy(extra);
+    }
+
+    @Override
+    public String uid() {
+        return  UUID.randomUUID().toString();
+    }
+}
